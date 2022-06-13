a4de8fd9e5a66783ce41b814a428b0f0c9a41f2c
tneiroz <tneiroz@naver.com>
2022-06-13 10:11:25 +0900

    언어

--- src/main/java/me/eun/config/RootConfig.java
+++ src/main/java/me/eun/config/RootConfig.java
@@ -1,13 +1,22 @@
 package me.eun.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
-@ComponentScan("me.eun")
 public class RootConfig {
 	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource source
+			= new ReloadableResourceBundleMessageSource();
+		source.setBasenames("classpath:/message/message");
+		source.setDefaultEncoding("utf-8");
+		return source;
+			
+	}
 	
-	
-	
+
 }
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<spring:message code="board.bno"/> <br>
+<spring:message code="board.title"/> <br>
+<spring:message code="board.content"/> <br>
+<spring:message code="board.writer"/> <br>
+
+</body>
+</html>
