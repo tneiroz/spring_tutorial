1fb97c0db2a90b9471666208a00b0c615a842209
tneiroz <tneiroz@naver.com>
2022-06-13 09:35:11 +0900

    튜토리얼

---
+++ src/main/java/me/eun/config/RootConfig.java
@@ -0,0 +1,13 @@
+package me.eun.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("me.eun")
+public class RootConfig {
+	
+	
+	
+	
+}
---
+++ src/main/java/me/eun/controller/HomeCotroller.java
@@ -0,0 +1,14 @@
+package me.eun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeCotroller {
+		
+		@GetMapping("/")
+		public String home() {
+			return "home";
+		}
+	}
+
